eaef2884518038534c7582c04a915dedfe969f55
mrliang <284355163@qq.com>
2017-08-31 17:07:59 +0800

    测试钉钉回调钩子

--- config-server/src/main/java/com/liang/config/ConfigApplication.java
+++ config-server/src/main/java/com/liang/config/ConfigApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
-
 	public static void main(String[] args) {
 		new SpringApplicationBuilder( ConfigApplication.class).web(true).run(args);
 	}
